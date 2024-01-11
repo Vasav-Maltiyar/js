cd5b9d2686d54309866353ccb115824ae41cff59
Vasav-Maltiyar <139356765+Vasav-Maltiyar@users.noreply.github.com>
2024-01-11 10:54:35 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,27 @@
+alert("This page consists of STUPIDITY open at your own risk");
+confirm("Warning u again!!!This page consists of STUPIDITY open at your own risk");
+var name = prompt("we can't let anyone in. What's your name?{the following website is very scary!!!!}");
+var age = prompt("we can't let anyone in. What's your age?{the following website is very scary!!!!}");
+alert("Hi "+ name + " ,your age is " + age +" .Our boss has allowed u to the website! BE CAREFUL WITH IT!!!!!!" );
+var x = parseInt(prompt("Enter first number(this test proves that u are a human):"));
+var y = parseInt(prompt("Enter second number:"));
+var z;
+z = Math.max(x,y);
+prompt("Mazimum no. is  " ,(z));
+var a = parseInt(prompt("Enter a square number(this test proves that u are a human):"));
+var sqrt;
+sqrt = Math.sqrt(a);
+prompt("Square root is  " ,(sqrt));
+var m = parseInt(prompt("Enter first number(this test proves that u are a human):"));
+var s = parseInt(prompt("Enter second number:"));
+var d;
+d = m+s;
+prompt("sum is  " ,(d));
+alert(Date())
+document.write("Hello World!\n I was just kidding hahahahahahahahaha!!!!! \n listen to the audio given and then reads the next line, this is a order !!!!!!!!!!!!!!!! ");
+document.write("The html of this website consisits of exactly 18 lines, 8-1=7 THALA FOR A REASON !!!!!");
+
+function displaymessage()
+{
+    alert(" the year is 2024; 2+0+2+4=8 . There our 8 planets in solar system . 8+8 = 16; 1+6 = 7 THALA FOR A RESON!!!!")
+}
